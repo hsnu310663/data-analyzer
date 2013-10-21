58525edf25749f0404c27b80a3bcd7dd8842b6c3
Ian Tsai <coolcool01@gmail.com>
2013-10-21 17:45:16 +0800

    New function-computing_center

---
+++ computing_center.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *fetchData() {
+  int *data;
+  // fetch data...
+  return data;
+}
+
+void processData(int data[]) {
+  // process data...
+  // improve process algorithm
+}
+
+void showData(int data[]) {
+  // print data...
+}
+
