5c5ef9130050be52ffb7dfa850eb305647c5c315
re330 <re3330@gmail.com>
2013-10-21 18:50:49 +0800

    analyzer_handler

---
+++ analyzer_handler.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <hadoop_api.c>
+
+int *fetchData() {
+  int *data;
+  // fetch data...
+  return data;
+}
+
+void processData(int data[]) {
+  // process data...
+  // improve process algorithm
+}
+
+void showData(int data[]) {
+  // print data...
+}
+
+
+void showData2(int data[]) {
+  // print data...
+}
+
+void showData3(int data[]) {
+  // print data...
+}
+
+void showData4(int data[]) {
+  // print data...
+}
