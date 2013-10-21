17ce6cb71d7a78390a059c24e43d75d092cc5cd3
kevin255466 <kevin255466@gmail.com>
2013-10-21 18:20:17 +0800

    first commit

---
+++ hadoop_api.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *fetchData() {
+  int *data;
+  // fetch data...
+  return data;
+}
+
+void processData(int data[]) {
+  // process data...
+  // improve process algorithm
+}
+
+void showData(int data[]) {
+  // print data...
+}
+
+
+void showData2(int data[]) {
+  // print data...
+}
